b3f8093a5f0773024042d6563d5d0e7f4b0c0710
ocnelias <ocnelias@gmail.com>
2021-03-21 17:46:18 +0200

    fix: refactor vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,29 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-/*
-   * Returns the result the Vigenère encryption on the given text with the given key.
-   */
-function crypt(input, key) {
-  var output = "";
-  for (var i = 0, j = 0; i < input.length; i++) {
-    var c = input.charCodeAt(i);
-    if (isUppercase(c)) {
-      output += String.fromCharCode((c - 65 + key[j % key.length]) % 26 + 65);
-      j++;
-    } else if (isLowercase(c)) {
-      output += String.fromCharCode((c - 97 + key[j % key.length]) % 26 + 97);
-      j++;
-    } else {
-      output += input.charAt(i);
-    }
-  }
-  return output;
-}
 
 function filterKey(key) {
-  var result = [];
-  for (var i = 0; i < key.length; i++) {
-    var c = key.charCodeAt(i);
+  let result = [];
+  for (let i = 0; i < key.length; i++) {
+    let c = key.charCodeAt(i);
     if (isLetter(c))
       result.push((c - 65) % 32);
   }
@@ -45,40 +26,66 @@ function isLowercase(c) {
   return 97 <= c && c <= 122;  // 97 is character code for 'a'. 122 is 'z'.
 }
 
-
-
 class VigenereCipheringMachine {
-   encrypt(message, key) {
-    if (key.length === 0) {
-      alert("Key is empty");
-      return;
-    }
 
-    var keyArray = filterKey(key);
-    if (keyArray.length === 0) {
-      alert("Key has no letters");
-      return;
-    }
-
-    return(crypt(message, keyArray).toUpperCase());
+  constructor(isReversed) {
+    this.isReversed = isReversed;
   }
 
-   decrypt(message, key) {
-    if (key.length === 0) {
-      alert("Key is empty");
-      return;
+  /*
+  * Returns the result the Vigenère encryption on the given text with the given key.
+  */
+  crypt(input, key) {
+    if (input === 'undefined' || key === 'undefined') {
+      throw Error('error arguments');
+    }
+    let output = "";
+    for (let i = 0, j = 0; i < input.length; i++) {
+      let c = input.charCodeAt(i);
+      if (isUppercase(c)) {
+        output += String.fromCharCode((c - 65 + key[j % key.length]) % 26 + 65);
+        j++;
+      } else if (isLowercase(c)) {
+        output += String.fromCharCode((c - 97 + key[j % key.length]) % 26 + 97);
+        j++;
+      } else {
+        output += input.charAt(i);
+      }
+    }
+    return output;
+  }
+
+  encrypt(message, key) {
+    if (message === 'undefined' || key === 'undefined') {
+      throw Error('error arguments');
     }
 
-    var keyArray = filterKey(key);
-    if (keyArray.length === 0) {
-      alert("Key has no letters");
-      return;
+    let keyArray = filterKey(key);
+
+    if (this.isReversed === false) {
+      return (this.crypt(message.split('').reverse().join(''), keyArray).toUpperCase());
+    } else {
+      return (this.crypt(message, keyArray).toUpperCase());
     }
 
-    for (var i = 0; i < keyArray.length; i++)
+  }
+
+  decrypt(message, key) {
+    if (message === 'undefined' || key === 'undefined') {
+      throw Error('error arguments');
+    }
+
+    let keyArray = filterKey(key);
+
+    for (let i = 0; i < keyArray.length; i++)
       keyArray[i] = (26 - keyArray[i]) % 26;
 
-    return(crypt(message, keyArray));
+    if (this.isReversed === false) {
+      return (this.crypt(message.split('').reverse().join(''), keyArray));
+    } else {
+      return (this.crypt(message, keyArray));
+    }
+
   }
 
 }
