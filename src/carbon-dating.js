5193ad41885d0feb1284c8e318b3ab96dfebc399
ocnelias <ocnelias@gmail.com>
2021-03-26 15:08:01 +0200

    feat: implement carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,18 +4,10 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-
-/*
-  return typeof sampleActivity;
-
-  //let num = parseInt(sampleActivity);
-
-  if (isNaN(sampleActivity) || sampleActivity === undefined) {
-    //return false;
-  } else {
-   // return 0.693 / 5730;
-  }
-*/
-
+  return /^\d+$|^\d+\.\d+$/.test(sampleActivity) &&
+  typeof sampleActivity === "string" &&
+  Number(sampleActivity) > 0 &&
+  Number(sampleActivity) < 15
+    ? Math.ceil(Math.log2(sampleActivity / MODERN_ACTIVITY) * -HALF_LIFE_PERIOD)
+    : false;
 };
