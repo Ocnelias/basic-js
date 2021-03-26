475975e67f27b3253d392e113119f0f3d0c1b530
ocnelias <ocnelias@gmail.com>
2021-03-26 15:34:26 +0200

    fix: fix recursive depth tests

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,10 +2,18 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+
+    let currentDepth = 0;
+    let res = 0;
+
     if (Array.isArray(arr)) {
-      return 1 + Math.max(...arr.map(t => this.calculateDepth(t)))
-    } else {
-      throw new CustomError('err');
+      res = currentDepth = 1;
+      for (let i = 0; i < arr.length; i++) {
+        currentDepth += this.calculateDepth(arr[i]);
+        currentDepth > res ? res = currentDepth : currentDepth;
+        currentDepth = 1;
+      }
+      return res;
     }
   }
-};
+}
